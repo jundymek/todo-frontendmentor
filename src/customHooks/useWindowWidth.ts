bc9590b8de7441fb56a3b12cedcf5ea4dabdbda8
jundymek <jundymek@gmail.com>
2021-04-18 16:42:10 +0200

    feat: useWindowWidth custom hook

---
+++ src/customHooks/useWindowWidth.ts
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+
+export function useWindowWidth() {
+  const [windowWidth, setWindowWidth] = useState<number>(0);
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowWidth(window.innerWidth);
+    }
+    window.addEventListener("resize", handleResize);
+    handleResize();
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return windowWidth;
+}
